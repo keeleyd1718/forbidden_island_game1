ce9a52bbc64f13079b71686c1503a3a8567f3c44
keeleyd1718 <dockter25@up.edu>
2023-12-13 22:02:16 -0800

    commented out all Collections.copy()

--- app/src/main/java/com/example/forbiddenislandgame/FiGameState.java
+++ app/src/main/java/com/example/forbiddenislandgame/FiGameState.java
@@ -265,21 +265,21 @@ public class FiGameState extends GameState {
     /** Copy Constructor */
     public FiGameState(FiGameState other){
         this.treasureDeck = other.treasureDeck;
-        Collections.copy(treasureDeck, other.treasureDeck);
+        //Collections.copy(treasureDeck, other.treasureDeck);
         this.discardTreasureDeck = other.discardTreasureDeck;
-        Collections.copy(discardTreasureDeck, other.discardTreasureDeck);
+        //Collections.copy(discardTreasureDeck, other.discardTreasureDeck);
         this.floodDeck = other.floodDeck;
-        Collections.copy(floodDeck, other.floodDeck);
+        //Collections.copy(floodDeck, other.floodDeck);
         this.drawnFloodCards= other.drawnFloodCards;
-        Collections.copy(drawnFloodCards, other.drawnFloodCards);
+        //Collections.copy(drawnFloodCards, other.drawnFloodCards);
         this.discardFloodDeck = other.discardFloodDeck;
-        Collections.copy(discardFloodDeck, other.discardFloodDeck);
+        //Collections.copy(discardFloodDeck, other.discardFloodDeck);
         this.humanPlayerHand = other.humanPlayerHand;
-        Collections.copy(humanPlayerHand, other.humanPlayerHand);
+        //Collections.copy(humanPlayerHand, other.humanPlayerHand);
         this.dumbAiHand = other.dumbAiHand;
-        Collections.copy(dumbAiHand, other.dumbAiHand);
+        //Collections.copy(dumbAiHand, other.dumbAiHand);
         this.smartAiHand = other.smartAiHand;
-        Collections.copy(smartAiHand, other.smartAiHand);
+        //Collections.copy(smartAiHand, other.smartAiHand);
         this.numPlayers = other.numPlayers;
         this.playerTurn = other.playerTurn;
         this.floodMeter = other.floodMeter;
@@ -289,25 +289,25 @@ public class FiGameState extends GameState {
         this.isCapturedOceanChalice = other.isCapturedOceanChalice;
         this.isCapturedWindStatue = other.isCapturedWindStatue;
         this.numEarthStoneCardsInHand = other.numEarthStoneCardsInHand;
-
+        //
         this.numFireCrystalCardsInHand = other.numFireCrystalCardsInHand;
-
+        //
         this.numOceanChaliceCardsInHand = other.numOceanChaliceCardsInHand;
-
+        //
         this.numWindStatueCardsInHand = other.numWindStatueCardsInHand;
-
+        //
         this.numHelicopterLiftCardsInHand = other.numHelicopterLiftCardsInHand;
-
+        //
         this.earthStoneTreasureCards = other.earthStoneTreasureCards;
-        Collections.copy(earthStoneTreasureCards, other.earthStoneTreasureCards);
+        //Collections.copy(earthStoneTreasureCards, other.earthStoneTreasureCards);
         this.fireCrystalTreasureCards = other.fireCrystalTreasureCards;
-        Collections.copy(fireCrystalTreasureCards, other.fireCrystalTreasureCards);
+        //Collections.copy(fireCrystalTreasureCards, other.fireCrystalTreasureCards);
         this.oceanChaliceTreasureCards = other.oceanChaliceTreasureCards;
-        Collections.copy(oceanChaliceTreasureCards, other.oceanChaliceTreasureCards);
+        //Collections.copy(oceanChaliceTreasureCards, other.oceanChaliceTreasureCards);
         this.windStatueTreasureCards = other.windStatueTreasureCards;
-        Collections.copy(windStatueTreasureCards, other.windStatueTreasureCards);
+        //Collections.copy(windStatueTreasureCards, other.windStatueTreasureCards);
         this.helicopterLiftCards = other.helicopterLiftCards;
-        Collections.copy(helicopterLiftCards, other.helicopterLiftCards);
+        //Collections.copy(helicopterLiftCards, other.helicopterLiftCards);
         this.player1Location = other.player1Location;
         this.player2Location = other.player2Location;
         this.player3Location = other.player3Location;
